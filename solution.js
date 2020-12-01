f31ea28fcb9659c55ff53efe7e23838f3614bf59
jamieberrier <berrier.jamie@gmail.com>
2020-12-01 13:08:31 -0800

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,18 @@
+// Given a non-negative integer num, return whether it is a palindrome.
+
+// Bonus: Can you solve it without using strings?
+
+// Constraints
+// num < 2 ** 31
+
+class Solution {
+  solve(num) {
+      const reverseNum = parseInt(Array.from(String(num), Number).reverse().join(""))
+  
+      if(num === reverseNum) {
+          return true
+      } else {
+          return false
+      }
+  }
+}
